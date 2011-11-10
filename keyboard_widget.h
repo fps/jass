41afb132aef2b327ea636a8278c665c0162d3817
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2011-11-10 02:02:25 +0100

    handle setting note ranges

--- keyboard_widget.h
+++ keyboard_widget.h
@@ -3,8 +3,12 @@
 
 #include <QWidget>
 #include <QPainter>
+#include <QMouseEvent>
+
+#include <iostream>
 
 #include "generator.h"
+#include "engine.h"
 
 struct keyboard_widget : public QWidget {
 	Q_OBJECT
@@ -17,7 +21,7 @@ struct keyboard_widget : public QWidget {
 			gen(gen)
 		{
 			QPalette palette = QWidget::palette();
-			palette.setColor(backgroundRole(), Qt::white);
+			//palette.setColor(backgroundRole(), Qt::white);
 			setPalette(palette);
 		}
 
@@ -36,29 +40,29 @@ struct keyboard_widget : public QWidget {
 				) {
 					painter.setPen(Qt::black);
 					painter.setBrush(Qt::black);
-				} else {
-					painter.setPen(Qt::white);
-					painter.setBrush(Qt::white);
+					painter.drawRect(width() * (double(i)/128), 0, width()/(double)128 - 1, 3.0*height()/4.0);
+				} 
+
+				if (
+					i % 12 == 5 ||
+					i % 12 == 0 
+				) {
+					painter.setPen(Qt::black);
+					painter.setBrush(Qt::black);
+					painter.drawLine(width() * double(i)/128 - 1, 0, width() * double(i)/128 - 1, 3.0 * height()/4.0); 
 				}
-
-				painter.drawRect(width() * (double(i)/128), 0, width()/(double)128, 3.0 * height() / 4.0);
-
-				if (gen->t.min_note <= i && i <= gen->t.max_note) {
-					painter.setPen(Qt::gray);
-					painter.setBrush(Qt::gray);
-					painter.drawRect(width() * (double(i)/128), 3.0*height()/4.0, width()/(double)128, height()/4.0);
-				}
-
+	
 				if (gen->t.note == i) {
-					painter.setPen(Qt::red);
-					painter.setBrush(Qt::red);
-					painter.drawRect(width() * (double(i)/128), 3.0*height()/4.0, width()/(double)128, height()/4.0);
+					painter.setPen(QColor(255, 0, 0, 63));
+					painter.setBrush(QColor(255, 0, 0, 63));
+					painter.drawRect(width() * (double(i)/128), 0, width()/(double)128 - 1, height());
 				}
 
-				painter.setPen(Qt::black);
-				painter.setBrush(Qt::black);
-				painter.drawLine(width() * double(i)/128, 0, width() * double(i)/128, height()); 
 			}
+
+			painter.setPen(QColor(0, 0, 255, 63));
+			painter.setBrush(QColor(0, 0, 255, 63));
+			painter.drawRect(width() * (double(gen->t.min_note)/128), 0, width() * (gen->t.max_note - gen->t.min_note)/(double)128 - 1, height());
 
 		}
 
@@ -69,8 +73,55 @@ struct keyboard_widget : public QWidget {
 
 		QSize sizeHint() const
 		{
-			return QSize(500, 10);
+			return QSize(800, 10);
 		}
+
+		int start_move;
+
+		void mousePressEvent(QMouseEvent *e) {
+			if (e->button() == Qt::LeftButton) {
+				if (e->modifiers() & Qt::ShiftModifier) 
+					gen->t.min_note = (double)(e->x())/width() * 128;
+				else {
+					gen->t.note = (double)(e->x())/width() * 128;
+				}
+			}
+			update();
+			e->accept();
+		}
+
+		void mouseMoveEvent(QMouseEvent *e) {
+			if ((e->buttons() & Qt::LeftButton) && (e->modifiers() & Qt::ShiftModifier)) {
+				gen->t.max_note = (double)(e->x())/width() * 128;
+			}
+			update();
+			e->accept();
+		}
+
+		void mouseReleaseEvent(QMouseEvent *e) {
+			if (e->modifiers() & Qt::ShiftModifier) {
+				e->accept();
+				return;
+			}
+
+			if (e->button() == Qt::LeftButton) {
+				gen->t.note = (double)(e->x())/width() * 128;
+				update();
+			}
+			update();
+			e->accept();
+		}
+
+		void mouseDoubleClickEvent(QMouseEvent *e) {
+			if (e->button() == Qt::LeftButton) {
+				gen->t.note = gen->t.min_note = gen->t.max_note = (double)(e->x())/width() * 128;
+				update();
+			}
+			update();
+			e->accept();
+		}
+
+		
 };
 
 #endif
